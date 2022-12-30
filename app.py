3a02645769c320d7a9191d2ad9c7e182bc102867
mohamad99927 <121675226+mohamad99927@users.noreply.github.com>
2022-12-30 16:04:00 +0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,118 @@
+from flask import Flask, render_template, Response
+import cv2
+import numpy as np
+import face_recognition
+import os
+from datetime import datetime
+import mimetypes
+
+app=Flask(__name__)
+
+# from PIL import ImageGrab
+
+path = 'Training_images'
+images = []
+classNames = []
+myList = os.listdir(path)
+print(myList)
+for cl in myList:
+    curImg = cv2.imread(f'{path}/{cl}')
+    images.append(curImg)
+    classNames.append(os.path.splitext(cl)[0])
+print(classNames)
+
+
+def findEncodings(images):
+    encodeList = []
+
+
+    for img in images:
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        encode = face_recognition.face_encodings(img)[0]
+        encodeList.append(encode)
+    return encodeList
+
+
+def markAttendance(name):
+    with open('Attendance.csv', 'r+') as f:
+        myDataList = f.readlines()
+
+
+        nameList = []
+        for line in myDataList:
+            entry = line.split(',')
+            nameList.append(entry[0])
+            if name not in nameList:
+                now = datetime.now()
+                dtString = now.strftime('%H:%M')
+                f.writelines(f'\n{name},{dtString}')
+
+#### FOR CAPTURING SCREEN RATHER THAN WEBCAM
+# def captureScreen(bbox=(300,300,690+300,530+300)):
+#     capScr = np.array(ImageGrab.grab(bbox))
+#     capScr = cv2.cvtColor(capScr, cv2.COLOR_RGB2BGR)
+#     return capScr
+
+encodeListKnown = findEncodings(images)
+print('Encoding Complete')
+
+camera = cv2.VideoCapture(0)
+
+
+def gen_mjpeg(): 
+ while True:
+    # Read the next frame from the webcam
+    success, img = camera.read()
+    
+    
+    # Only process every other frame
+    
+# img = captureScreen()
+    imgS = cv2.resize(img, (0, 0), None, 0.25, 0.25)
+    imgS = cv2.cvtColor(imgS, cv2.COLOR_BGR2RGB)
+
+    facesCurFrame = face_recognition.face_locations(imgS)
+    encodesCurFrame = face_recognition.face_encodings(imgS, facesCurFrame)
+
+    for encodeFace, faceLoc in zip(encodesCurFrame, facesCurFrame):
+        matches = face_recognition.compare_faces(encodeListKnown, encodeFace)
+        faceDis = face_recognition.face_distance(encodeListKnown, encodeFace)
+# print(faceDis)
+        matchIndex = np.argmin(faceDis)
+        
+        if matches[matchIndex]:
+            name = classNames[matchIndex].upper()
+       
+
+# print(name)
+            y1, x2, y2, x1 = faceLoc
+            y1, x2, y2, x1 = y1 * 4, x2 * 4, y2 * 4, x1 * 4
+            cv2.rectangle(img, (x1, y1), (x2, y2), (0, 0, 255), 2)
+            cv2.rectangle(img, (x1, y2 - 35), (x2, y2), (0, 0, 255), cv2.FILLED)
+            cv2.putText(img, name, (x1 + 6, y2 - 6), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 2)
+            markAttendance(name)
+
+    cv2.imshow('video', img)
+    cv2.waitKey(1)
+
+    ret, jpeg = cv2.imencode('.jpg', img)
+
+        # Check if the frame was successfully encoded
+    if not ret:
+            continue
+
+        # Yield the encoded frame
+    yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + jpeg.tobytes() + b'\r\n')
+    
+
+
+    
+@app.route('/')
+def index():
+    return render_template('index.html')
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen_mjpeg(), mimetype='multipart/x-mixed-replace; boundary=frame')
+if __name__=='__main__':
+    app.run(debug=True)    
